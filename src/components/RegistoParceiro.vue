ab308efa21b30e82eb43f0de9f59686a5c2278eb
Frederico Regateiro <frederico.regateiro@outlook.com>
2022-09-30 14:20:42 +0100

    fixes #64 Adicionar Telefone no registo do parceiro local

--- src/components/RegistoParceiro.vue
+++ src/components/RegistoParceiro.vue
@@ -1,25 +1,9 @@
 <template>
   <q-card bordered class="card-login">
     <q-card-section>
-      <q-tabs
-        v-model="tab"
-        outside-arrows
-        inline-label
-        mobile-arrows
-        dense
-        align="center"
-        narrow-indicator
-      >
-        <q-tab
-          name="geral"
-          :icon="mdiBadgeAccountHorizontalOutline"
-          :label="$t('html.registerPartner.tabMain')"
-        />
-        <q-tab
-          name="descricao"
-          :icon="mdiFileDocumentEditOutline"
-          :label="$t('html.registerPartner.tabDescription')"
-        />
+      <q-tabs v-model="tab" outside-arrows inline-label mobile-arrows dense align="center" narrow-indicator>
+        <q-tab name="geral" :icon="mdiBadgeAccountHorizontalOutline" :label="$t('html.registerPartner.tabMain')" />
+        <q-tab name="descricao" :icon="mdiFileDocumentEditOutline" :label="$t('html.registerPartner.tabDescription')" />
       </q-tabs>
 
       <q-separator />
@@ -30,136 +14,55 @@
             <div class="text-h6 col-12">
               {{ $t('html.registerPartner.user') }}
             </div>
-            <q-input
-              v-model="empresa.username"
-              outlined
-              :label="$t('html.registerPartner.username')"
-              class="col-xs-12 col-md-6"
-              :rules="[isUsernamevalid]"
-              ref="inputUserName"
-            >
+            <q-input v-model="empresa.username" outlined :label="$t('html.registerPartner.username')" class="col-xs-12 col-md-6" :rules="[isUsernamevalid]" ref="inputUserName">
               <template v-if="empresa.username" v-slot:append>
-                <q-icon
-                  :name="mdiCloseCircle"
-                  @click.stop="empresa.username = null"
-                  class="cursor-pointer"
-                />
+                <q-icon :name="mdiCloseCircle" @click.stop="empresa.username = null" class="cursor-pointer" />
               </template>
             </q-input>
-            <q-input
-              v-model="empresa.password"
-              outlined
-              :type="isPwd ? 'password' : 'text'"
-              :label="$t('html.registerPartner.userPassword')"
-              class="col-xs-12 col-md-6"
-              :rules="[isPasswordValid]"
-              ref="inputPassword"
-            >
+            <q-input v-model="empresa.password" outlined :type="isPwd ? 'password' : 'text'" :label="$t('html.registerPartner.userPassword')" class="col-xs-12 col-md-6" :rules="[isPasswordValid]" ref="inputPassword">
               <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? mdiEyeOff : mdiEye"
-                  class="cursor-pointer"
-                  aria-hidden="true"
-                  @click="isPwd = !isPwd"
-                />
-                <q-icon
-                  v-if="empresa.password"
-                  :name="mdiCloseCircle"
-                  @click.stop="empresa.password = null"
-                  class="cursor-pointer"
-                />
+                <q-icon :name="isPwd ? mdiEyeOff : mdiEye" class="cursor-pointer" aria-hidden="true" @click="isPwd = !isPwd" />
+                <q-icon v-if="empresa.password" :name="mdiCloseCircle" @click.stop="empresa.password = null" class="cursor-pointer" />
               </template>
             </q-input>
             <div class="text-h6 col-12">
               {{ $t('html.registerPartner.organization') }}
             </div>
-            <q-input
-              v-model="empresa.nome"
-              outlined
-              :rules="[isNameValid]"
-              ref="inputName"
-              :label="$t('html.registerPartner.name')"
-              class="col-xs-12 col-md-6"
-            />
-            <q-input
-              v-model="empresa.email"
-              outlined
-              :rules="[isEmailRule]"
-              ref="inputEmail"
-              :label="$t('html.registerPartner.contactEmail')"
-              class="col-xs-12 col-md-6"
-            />
+            <q-input v-model="empresa.nome" outlined :rules="[isNameValid]" ref="inputName" :label="$t('html.registerPartner.name')" class="col-xs-12 col-md-6" />
+            <q-input v-model="empresa.email" outlined :rules="[isEmailRule]" ref="inputEmail" :label="$t('html.registerPartner.contactEmail')" class="col-xs-12 col-md-6" />
 
-            <TipoProjetoSelector
-              :tipos="tiposProjeto"
-              :tipo="empresa.tipos_projeto"
-              :lang="lang"
-              class="col-xs-12"
-            />
+            <TipoProjetoSelector :tipos="tiposProjeto" :tipo="empresa.tipos_projeto" :lang="lang" class="col-xs-12" />
 
-            <q-select
-              v-model="empresa.pais_id"
-              :options="paises"
-              :label="$t('html.registerPartner.country')"
-              :option-label="lang === 'pt' ? 'nome' : 'nome_en'"
-              option-value="id"
-              @filter="filterFn"
-              :rules="[isCountryValid]"
-              ref="inputCountry"
-              use-input
-              emit-value
-              map-options
-              outlined
-              class="col-xs-12"
-            />
+            <q-select v-model="empresa.pais_id" :options="paises" :label="$t('html.registerPartner.country')" :option-label="lang === 'pt' ? 'nome' : lang === 'fr' ? 'nome_fr' : 'nome_en'" option-value="id" @filter="filterFn" :rules="[isCountryValid]" ref="inputCountry" use-input emit-value map-options outlined class="col-xs-12" />
 
-            <q-input
-              v-model="empresa.website"
-              outlined
-              label="Web"
-              class="col-xs-12 col-md-4"
-            >
+            <q-input v-model="empresa.telefone" outlined label="Telefone" class="col-xs-12 col-md-6">
+              <template v-slot:append>
+                <q-icon :name="mdiPhoneClassic" color="primary" />
+              </template>
+            </q-input>
+            <q-input v-model="empresa.website" outlined label="Web" class="col-xs-12 col-md-6">
               <template v-slot:append>
                 <q-icon :name="mdiWeb" color="primary" />
               </template>
             </q-input>
-            <q-input
-              v-model="empresa.facebook"
-              outlined
-              label="Facebook"
-              class="col-xs-12 col-md-4"
-            >
+            <q-input v-model="empresa.facebook" outlined label="Facebook" class="col-xs-12 col-md-4">
               <template v-slot:append>
                 <q-icon :name="mdiFacebook" color="primary" />
               </template>
             </q-input>
-            <q-input
-              v-model="empresa.twitter"
-              outlined
-              label="Twitter"
-              class="col-xs-12 col-md-4"
-            >
+            <q-input v-model="empresa.twitter" outlined label="Twitter" class="col-xs-12 col-md-4">
               <template v-slot:append>
                 <q-icon :name="mdiTwitter" color="primary" />
               </template>
             </q-input>
-            <q-input
-              v-model="empresa.linkedin"
-              outlined
-              label="LinkedIn"
-              class="col-xs-12 col-md-4"
-            >
+            <q-input v-model="empresa.linkedin" outlined label="LinkedIn" class="col-xs-12 col-md-4">
               <template v-slot:append>
                 <q-icon :name="mdiLinkedin" color="primary" />
               </template>
             </q-input>
 
             <div class="col-xs-12 col-md-6">
-              <q-file
-                outlined
-                v-model="empresa.logo"
-                :label="$t('html.registerPartner.logo')"
-              >
+              <q-file outlined v-model="empresa.logo" :label="$t('html.registerPartner.logo')">
                 <template v-slot:prepend>
                   <q-icon :name="mdiImageSearchOutline" />
                 </template>
@@ -181,14 +84,7 @@
                 },
                 'fullscreen'
               ],
-              [
-                'bold',
-                'italic',
-                'strike',
-                'underline',
-                'subscript',
-                'superscript'
-              ],
+              ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
               ['token', 'hr', 'link', 'custom_btn'],
               [
                 {
@@ -203,32 +99,14 @@
                   fixedLabel: true,
                   fixedIcon: true,
                   list: 'no-icons',
-                  options: [
-                    'size-1',
-                    'size-2',
-                    'size-3',
-                    'size-4',
-                    'size-5',
-                    'size-6',
-                    'size-7'
-                  ]
+                  options: ['size-1', 'size-2', 'size-3', 'size-4', 'size-5', 'size-6', 'size-7']
                 },
                 {
                   label: $q.lang.editor.defaultFont,
                   icon: $q.iconSet.editor.font,
                   fixedIcon: true,
                   list: 'no-icons',
-                  options: [
-                    'default_font',
-                    'arial',
-                    'arial_black',
-                    'comic_sans',
-                    'courier_new',
-                    'impact',
-                    'lucida_grande',
-                    'times_new_roman',
-                    'verdana'
-                  ]
+                  options: ['default_font', 'arial', 'arial_black', 'comic_sans', 'courier_new', 'impact', 'lucida_grande', 'times_new_roman', 'verdana']
                 },
                 'removeFormat'
               ],
@@ -257,43 +135,14 @@
 
     <q-card-actions align="right">
       <LanguageSelector @language_changed="languageChanged" />
-      <q-btn
-        :label="$t('html.registerPartner.return')"
-        type="reset"
-        rounded
-        flat
-        @click="onCancel"
-      />
-      <q-btn
-        color="primary"
-        :label="$t('html.registerPartner.register')"
-        rounded
-        class="text-white action-btn"
-        :icon="mdiLogin"
-        size="lg"
-        @click="onRegister"
-        :loading="onCreatingAcount"
-      />
+      <q-btn :label="$t('html.registerPartner.return')" type="reset" rounded flat @click="onCancel" />
+      <q-btn color="primary" :label="$t('html.registerPartner.register')" rounded class="text-white action-btn" :icon="mdiLogin" size="lg" @click="onRegister" :loading="onCreatingAcount" />
     </q-card-actions>
   </q-card>
 </template>
 
 <script>
-import {
-  mdiEyeOff,
-  mdiEye,
-  mdiLogin,
-  mdiCloseCircle,
-  mdiWeb,
-  mdiFacebook,
-  mdiTwitter,
-  mdiLinkedin,
-  mdiBadgeAccountHorizontalOutline,
-  mdiFileDocumentEditOutline,
-  mdiImageSearchOutline,
-  mdiCellphone,
-  mdiPhoneClassic
-} from '@quasar/extras/mdi-v6'
+import { mdiEyeOff, mdiEye, mdiLogin, mdiCloseCircle, mdiWeb, mdiFacebook, mdiTwitter, mdiLinkedin, mdiBadgeAccountHorizontalOutline, mdiFileDocumentEditOutline, mdiImageSearchOutline, mdiCellphone, mdiPhoneClassic } from '@quasar/extras/mdi-v6'
 import { defineComponent, ref, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useQuasar } from 'quasar'
@@ -325,6 +174,7 @@ export default defineComponent({
           facebook: null,
           twitter: null,
           linkedin: null,
+          telefone: null,
           logo: null,
           pais_id: null
         },
@@ -334,9 +184,7 @@ export default defineComponent({
     onMounted(async () => {
       try {
         tiposProjeto.value = await get('tiposprojeto/read-ativo.php')
-        empresa.value.tipos_projeto = [
-          tiposProjeto.value.filter((t) => t.pai_id > 0)[0].id
-        ]
+        empresa.value.tipos_projeto = [tiposProjeto.value.filter((t) => t.pai_id > 0)[0].id]
       } catch {
         $q.notify({
           message: t('html.errors.getProjectTypes'),
@@ -377,13 +225,7 @@ export default defineComponent({
     }
     const onRegister = async () => {
       // do login
-      if (
-        inputUserName.value.validate() &&
-        inputPassword.value.validate() &&
-        inputName.value.validate() &&
-        inputEmail.value.validate() &&
-        inputCountry.value.validate()
-      ) {
+      if (inputUserName.value.validate() && inputPassword.value.validate() && inputName.value.validate() && inputEmail.value.validate() && inputCountry.value.validate()) {
         onCreatingAcount.value = true
         // account has been created,
         const data = new FormData()
@@ -401,6 +243,7 @@ export default defineComponent({
         data.append('facebook', empresa.value.facebook)
         data.append('twitter', empresa.value.twitter)
         data.append('linkedin', empresa.value.linkedin)
+        data.append('telefone', empresa.value.telefone)
         try {
           const result = await postForm('empresas/create-parceiro.php', data)
           if (result.ok === true) {
