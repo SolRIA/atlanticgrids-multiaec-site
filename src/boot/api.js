c05fdccfd253d73071454fcabb8e3cd549b1959d
Frederico Regateiro <frederico.regateiro@solria.pt>
2023-06-08 14:32:11 +0100

    change api key header

--- src/boot/api.js
+++ src/boot/api.js
@@ -1,47 +1,66 @@
-import { Notify } from 'quasar'
-import { route } from 'quasar/wrappers'
+import { Notify } from 'quasar';
+import { route } from 'quasar/wrappers';
 
 export function apiBaseUrl() {
-  return process.env.DEV ? 'http://localhost:8000/api/' : 'https://multiaecapi.ptpc.pt/api/'
+  return process.env.DEV
+    ? 'http://localhost:8000/api/'
+    : 'https://multiaecapi.ptpc.pt/api/';
 }
 
 export function apiPublicUrl(name) {
   if (typeof name === 'undefined' || name === null) {
-    return null
+    return null;
   }
-  return process.env.DEV ? `http://localhost:8000/public/${name}` : `https://multiaecapi.ptpc.pt/public/${name}`
+  return process.env.DEV
+    ? `http://localhost:8000/public/${name}`
+    : `https://multiaecapi.ptpc.pt/public/${name}`;
 }
 
 export function createQueryParameters(data) {
-  return `?${new URLSearchParams(data)}`
+  return `?${new URLSearchParams(data)}`;
 }
 
 export function getAuthToken() {
-  const currentUser = JSON.parse(localStorage.getItem('token'))
-  return 'KEY ' + currentUser.token
+  const currentUser = JSON.parse(localStorage.getItem('token'));
+  return 'KEY ' + currentUser.token;
 }
 
 export async function deleteAuth(url, data) {
-  return await baseApiAuth('DELETE', url, JSON.stringify(data), 'application/json')
+  return await baseApiAuth(
+    'DELETE',
+    url,
+    JSON.stringify(data),
+    'application/json'
+  );
 }
 
 export async function postAuth(url, data) {
-  return await baseApiAuth('POST', url, JSON.stringify(data), 'application/json')
+  return await baseApiAuth(
+    'POST',
+    url,
+    JSON.stringify(data),
+    'application/json'
+  );
 }
 
 export async function postFormAuth(url, data) {
-  return await baseApiAuth('POST', url, data)
+  return await baseApiAuth('POST', url, data);
 }
 
 export async function getAuth(url, data) {
-  return await baseApiAuth('GET', url, JSON.stringify(data), 'application/json')
+  return await baseApiAuth(
+    'GET',
+    url,
+    JSON.stringify(data),
+    'application/json'
+  );
 }
 
 export function downloadXml(method, url, filename, data, elementid) {
-  return download(method, url, filename, data, 'application/xml', elementid)
+  return download(method, url, filename, data, 'application/xml', elementid);
 }
 export function downloadPdf(method, url, filename, data, elementid) {
-  return download(method, url, filename, data, 'application/pdf', elementid)
+  return download(method, url, filename, data, 'application/pdf', elementid);
 }
 function download(method, url, filename, data, mimeType, elementid) {
   return new Promise((resolve, reject) => {
@@ -51,60 +70,72 @@ function download(method, url, filename, data, mimeType, elementid) {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Credentials': true,
-        authorization: getAuthToken()
+        authorization: getAuthToken(),
       },
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data),
+    };
 
     fetch(apiBaseUrl() + url, config)
       .then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data], { type: mimeType }))
-        const link = document.getElementById(elementid)
-        link.href = url
-        link.setAttribute('download', filename)
-        resolve()
+        const url = window.URL.createObjectURL(
+          new Blob([response.data], { type: mimeType })
+        );
+        const link = document.getElementById(elementid);
+        link.href = url;
+        link.setAttribute('download', filename);
+        resolve();
       })
       .catch((error) => {
-        let message = 'Não foi possível concluir o pedido'
-        if (typeof error !== 'undefined' && typeof error.response !== 'undefined' && typeof error.response.data !== 'undefined' && typeof error.response.data.message !== 'undefined') {
-          message = error.response.data.message
+        let message = 'Não foi possível concluir o pedido';
+        if (
+          typeof error !== 'undefined' &&
+          typeof error.response !== 'undefined' &&
+          typeof error.response.data !== 'undefined' &&
+          typeof error.response.data.message !== 'undefined'
+        ) {
+          message = error.response.data.message;
         }
-        Notify.create({ message: message, type: 'negative' })
-        reject(error)
-      })
-  })
+        Notify.create({ message: message, type: 'negative' });
+        reject(error);
+      });
+  });
 }
 
 export async function baseApiAuth(method, url, data, contentType) {
-  var headers = new Headers()
-  headers.append('Access-Control-Allow-Credentials', 'true')
-  headers.append('Authorization', getAuthToken())
+  var headers = new Headers();
+  headers.append('Access-Control-Allow-Credentials', 'true');
+  headers.append('X_API_KEY', getAuthToken());
   if (typeof contentType !== 'undefined' && contentType !== null) {
-    headers.append('Content-Type', contentType)
+    headers.append('Content-Type', contentType);
   }
   const config = {
     method: method,
     headers: headers,
-    body: data
-  }
+    body: data,
+  };
 
   try {
-    let response = await fetch(apiBaseUrl() + url, config)
+    let response = await fetch(apiBaseUrl() + url, config);
 
     if (response.ok === false && response.status === 401) {
-      route.push('/login')
+      route.push('/login');
     } else {
-      return await response.json()
+      return await response.json();
     }
   } catch (error) {
-    console.error(error)
-    let message = 'Não foi possível concluir o pedido'
-    if (typeof error !== 'undefined' && typeof error.response !== 'undefined' && typeof error.response.data !== 'undefined' && typeof error.response.data.message !== 'undefined') {
-      message = error.response.data.message
+    console.error(error);
+    let message = 'Não foi possível concluir o pedido';
+    if (
+      typeof error !== 'undefined' &&
+      typeof error.response !== 'undefined' &&
+      typeof error.response.data !== 'undefined' &&
+      typeof error.response.data.message !== 'undefined'
+    ) {
+      message = error.response.data.message;
     }
-    Notify.create({ message: message, type: 'negative' })
+    Notify.create({ message: message, type: 'negative' });
 
-    return null
+    return null;
   }
 }
 
@@ -112,37 +143,37 @@ export async function post(url, data) {
   const config = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data)
-  }
+    body: JSON.stringify(data),
+  };
 
-  var response = await fetch(apiBaseUrl() + url, config)
+  var response = await fetch(apiBaseUrl() + url, config);
 
-  return await response.json()
+  return await response.json();
 }
 
 export async function postForm(url, data) {
   const config = {
     method: 'POST',
-    body: data
-  }
+    body: data,
+  };
 
-  var response = await fetch(apiBaseUrl() + url, config)
+  var response = await fetch(apiBaseUrl() + url, config);
 
-  return await response.json()
+  return await response.json();
 }
 
 export async function get(url, data) {
   const config = {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data)
-  }
+    body: JSON.stringify(data),
+  };
 
-  var response = await fetch(apiBaseUrl() + url, config)
+  var response = await fetch(apiBaseUrl() + url, config);
 
-  return await response.json()
+  return await response.json();
 }
